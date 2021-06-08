c93b4785dafea3a33af4c9192e7d4ec1cef0fdff
Milind-gupta <68066126+Milind-gupta@users.noreply.github.com>
2021-06-08 16:05:45 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,9 +1,7 @@
 provider "aws" {
-  profile = "testing"
-  shared_credentials_file = "/home/ubuntu/.aws/credentials"
-  region  = "ap-south-1"
+region  = "ap-south-1"
 }
 resource "aws_instance" "terraform" {
-  ami           = "ami-04bde106886a53080"
-  instance_type = "t2.micro"
+ami           = "ami-04bde106886a53080"
+instance_type = "t2.micro"
 }
