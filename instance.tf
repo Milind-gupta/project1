9e54bf730603f9d7cdd398c398753edab62b0723
Milind-gupta <68066126+Milind-gupta@users.noreply.github.com>
2021-06-10 13:11:01 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -6,11 +6,36 @@
     #}
   #}
 #}
-provider "aws" {
+#provider "aws" {
 #profile = "terraform"  
-region = "ap-south-1"
+#region = "ap-south-1"
+#}
+#resource "aws_instance" "terraform" {
+#ami           = "ami-04bde106886a53080"
+#instance_type = "t2.micro"
+#}
+provider "aws" {
+    profile = "default"
+    region = "ap-south-1"
+    }
+    resource "aws_instance" "terraform" {
+    ami = "ami-04bde106886a53080"
+    instance_type = "t2.micro"
+    key_name = aws_key_pair.project1.key_name
+  }
+    provisioner "remote-exec" {
+    connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("project1")
+    host        = aws_instance.terraform.public_ip
+  }
+    inline = [
+      "sudo apt update -y && upgrade -y",
+      "sudo apt install software-properties-common -y",
+      "sudo add-apt-repository --yes --update ppa:ansible/ansible",
+      "sudo apt install ansible -y"
+    ]
+  }
 }
-resource "aws_instance" "terraform" {
-ami           = "ami-04bde106886a53080"
-instance_type = "t2.micro"
-}
+
